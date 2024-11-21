7a1f03e3ca1631c3c482e1cdf2cb61a99c37e039
lupeng918 <lupeng918@126.com>
2024-11-21 10:50:38 +0800

    Update 环形矩阵.cpp
    添加注释

--- 环形矩阵.cpp
+++ 环形矩阵.cpp
@@ -6,12 +6,15 @@ int main(){
     int arr[100][100] = {};
     // x,y 表示数组元素的索引，num 表示要填写的数字，n 表示数组的宽度
     int x = 0, y = 0, num = 1, n;
+    // 数组 dir 保存了 4 个数组，分别代表向右、下、左、上行走时 x 和 y 索引的增量
     int dir[][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    int ind = 0;
+    int ind = 0; // ind 当做索引来获取不同方向的增量
     cin >> n;
+    //把填写过数字的行和列想象成走过的道路，up/down/left/right 分别代表上下左右已走路程的行列索引
     int left = 0, up = 0, right = n - 1, down = n - 1;
+    // max_n 为所填数字的最大值
     int max_n = n * n;
-
+    //循环填写数字，直到填写到最大数字后停止
     while (num <= max_n){
         arr[x][y] = num++;
         x += dir[ind][0];
